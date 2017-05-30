709fb9e0cf0d8d7abdc737f489e2286902786abe
BRVNDXN <brandon.thompson@auth0.com>
2017-05-29 18:55:34 -0700

    update API middleware

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ const checkJwt = jwt({
 
 const checkScopes = jwtAuthz([ 'read:messages' ]);
 
-app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+// app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+app.get('/api/private', checkJwt, function(req, res) {
   res.json({ 
     message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." 
   });
